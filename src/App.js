ba9c753f5267b1420366cd1c8ffedf51a64f55ad
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-01 17:41:00 +0545

    made dom search

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import { robots } from './robots'
 import SearchBox from './SearchBox';
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             robots: robots,
             searchfield: ''
         }
     }
-    handleSearchChange(event){
-        console.log(event.target.value)
+    handleSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+      
     }
 
     render() {
+        const filterdRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return (
             <div className="tc">
                 <h1>Robo Friends</h1>
-                <SearchBox searchChange={this.handleSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <SearchBox searchChange={this.handleSearchChange} />
+                <CardList robots={filterdRobots} />
             </div>
         )
     }
