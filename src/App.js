d9f38a3021dad73df093cedfc09a8d4cdffb7842
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-01 19:24:23 +0545

    fetched the robot data from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots'
 import SearchBox from './SearchBox';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users =>this.setState({robots: users}))
+    }
+
     handleSearchChange = (event) => {
         this.setState({ searchfield: event.target.value });
       
@@ -20,9 +26,10 @@ class App extends Component {
         const filterdRobots = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+        if(this.state.robots.length === 0){return <h1>Loading</h1>}
         return (
             <div className="tc">
-                <h1>Robo Friends</h1>
+                <h1 className="f1">Robo Friends</h1>
                 <SearchBox searchChange={this.handleSearchChange} />
                 <CardList robots={filterdRobots} />
             </div>
